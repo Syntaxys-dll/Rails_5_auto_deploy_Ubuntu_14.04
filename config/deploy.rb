36ae8fb7869812dbd973f8310b352b71aebfdd1f
Viacheslav Soldatov <syntaxys0dll@gmail.com>
2016-07-25 18:14:16 +0300

    Fix for capistrano console: remove 'bin' from linked_dirs in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :deploy_to, '/home/deploy/applications/test'
 
 set :log_level, :info
 set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
